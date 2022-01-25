7c1f8c2a42d4434db4c7d3bc31a3f48e084bba58
Kent Pedersen <webzone@kpdesign.dk>
2022-01-25 22:15:14 +0100

    added sensorship with *ngIf

--- src/app/tweets/tweets.component.ts
+++ src/app/tweets/tweets.component.ts
@@ -2,7 +2,7 @@
 import { Tweet } from './../interfaces/tweet';
 import { Component, OnInit } from '@angular/core';
 //Importing the service
-import{TweetSupplierService} from '../services/tweetSupplier.service';
+import { TweetSupplierService } from '../services/tweetSupplier.service';
 
 @Component({
   selector: 'app-tweets',
@@ -11,15 +11,38 @@ import{TweetSupplierService} from '../services/tweetSupplier.service';
 })
 export class TweetsComponent implements OnInit {
   tweets: Tweet[] = [];
-  name:string="TheRealDonaldDuck";
+  name: string = "TheRealDonaldDuck";
+  show: boolean = false;
+  buzzWords: string[] = ["Fake", "fake"];
+  thisIsSensored: string ="Denne besked er sensorerer, da den indeholder falsk information, og derfor er sensoreret af det diktatoriske Sociale medie!";
+
   //Initializing the service
   constructor(private tweetsService: TweetSupplierService) { }
 
   ngOnInit(): void {
     //Getting the tweets from the service and adding them to the array tweets
-    this.tweetsService.requestTweets().subscribe((tweet:Tweet[])=>{
+    this.tweetsService.requestTweets().subscribe((tweet: Tweet[]) => {
       next: this.tweets = tweet;
     });
   }
 
+  checkBuzzWords(index: number): boolean {
+    let tweet: string = this.tweets[index].text;
+
+    for (let i = 0; i < this.buzzWords.length; i++) {
+      if (!tweet.includes(this.buzzWords[i])) {
+        this.show = true;
+      }
+      if (tweet.includes(this.buzzWords[i])) {
+        i = this.buzzWords.length;
+        this.show = false;
+      };
+    }
+    return this.show;
+
+  }
+
 }
+
+
+
